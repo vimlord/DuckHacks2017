55062ee6c6cb23afab275f7001a1a3536615e07d
gluaxspeed <jonathan.t.pavlik@gmail.com>
2017-03-25 16:44:58 -0400

    pushing updated file

--- youtube.py
+++ youtube.py
@@ -1,4 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
-def search():
+def search(keyword):
     driver = webdriver.Chrome()
+    driver.get('http://www.pandora.com/')
+    search_bar = driver.find_element_by_id('masthead-search-term')
+    search_bar.send_keys(keyword + " music playlist")
+    search_bar.send_keys(Keys.ENTER)
+    links = driver.find_elements_by_xpath('//*[@href]')
+    links.click()
+    time.sleep(3000)
+    #driver.quit()
+
+
+search("happy")
