61fea344bf980607d6a3d2289526c041c72c5d42
Taylor He <taylor.he7@gmail.com>
2017-03-25 17:54:57 -0400

    This is cool

--- youtube.py
+++ youtube.py
@@ -5,14 +5,14 @@ import time
 def search(keyword):
     driver = webdriver.Chrome()
     driver.get('http://www.pandora.com/')
-    #time.sleep(10)
+    time.sleep(2)
     driver.find_element_by_class_name('SearchField__placeholder').click()
     search_bar = driver.find_element_by_class_name('SearchField__input')
     search_bar.send_keys(keyword)
     time.sleep(5)
     search_bar.send_keys(Keys.ENTER)
 
-    time.sleep(3000)
+    time.sleep()
     driver.quit()
 
 
