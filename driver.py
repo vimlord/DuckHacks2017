0239f1cdbb8107965187197bc08ef7e0d7da9a8e
Christopher Hittner <chrishittner@gmail.com>
2017-03-25 23:54:58 -0400

    Tweaks

--- driver.py
+++ driver.py
@@ -12,18 +12,23 @@ genres = ['pop', 'edm', 'rock']
 clf = loadClassifier('./classifier.pkl')
 
 def runCycle(prevState):
-    
+    print("Beginning procedure...") 
+
     # Takes a selfie.
     picture()
+    print("Snap!")
     
     # Gets the emotion data for the selfie.
     data = imageRequest('./selfie.jpg')
+    print("Scanned image.")
 
     # Gets the emotion vector from the image.
     vec = parseEmotion(data)
+    print("Acquired sentiment.")
 
     # Computes the prediction.
     pred = makePrediction(clf, vec)
+    print("Choice vector computed.")
 
     # Choose the dimension with the highest value.
     choice = 0
@@ -38,7 +43,7 @@ def runCycle(prevState):
 
     return genres[choice]
 
-
+runCycle('hi')
 
 
 
